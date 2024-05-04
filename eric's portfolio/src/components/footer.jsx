30dd6fc719ce64f16a34dda615f4a121ac83edc9
elindstr <elindstr@gmail.com>
2024-05-03 20:02:05 -0700

    update footer & header

---
+++ eric's portfolio/src/components/footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+
+    return (
+        <>
+            <a href="mailto:elindstr@gmail.com">Email</a>
+            <a href="https://github.com/elindstr">GitHub</a>
+            <a href="https://www.linkedin.com/in/eric-lindstrom-24439b27/">LinkedIn</a>
+            <a href="https://l2c2p.com/attorneys/eric-lindstrom">Eric the Lawyer</a>
+        </>
+    )
+}
--- eric's portfolio/src/components/header.jsx
+++ eric's portfolio/src/components/header.jsx
@@ -1,13 +1,11 @@
 import { NavLink } from 'react-router-dom';
 
-
-function Header() {
+export default function Header() {
     
     return (
         <>
             <nav>
-                <NavLink to="/" className={({ isActive }) => isActive ? 'nav nav-link-active' : 'nav nav-link'}>Home</NavLink>
-                <NavLink to="/About" className={({ isActive }) => isActive ? 'nav nav-link-active' : 'nav nav-link'}>About Me</NavLink>
+                <NavLink to="/" className={({ isActive }) => isActive ? 'nav nav-link-active' : 'nav nav-link'}>About Me</NavLink>
                 <NavLink to="/Portfolio" className={({ isActive }) => isActive ? 'nav nav-link-active' : 'nav nav-link'}>Portfolio</NavLink>
                 <NavLink to="/Contact" className={({ isActive }) => isActive ? 'nav nav-link-active' : 'nav nav-link'}>Contact</NavLink>
                 <NavLink to="/Resume" className={({ isActive }) => isActive ? 'nav nav-link-active' : 'nav nav-link'}>Resume</NavLink>
@@ -17,5 +15,3 @@ function Header() {
         </>
     )
 }
-
-export default Header
