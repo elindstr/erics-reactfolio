51f01902d8e76d436190cdcdf1e9ad2e08decca5
elindstr <elindstr@gmail.com>
2024-05-05 22:54:31 -0700

    attempting to fix download link bugging on deployed site

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,10 +1,10 @@
-const pdfUrl = "../assets/resume.pdf"
+const pdfUrl = "resume.pdf"
 
 export default function Resume() {        
     return (
         <div>
             <h1>Resume</h1>
-            <a href={pdfUrl} download="resume.pdf">
+            <a href="resume.pdf" download="resume.pdf">
                 <button>Download PDF</button>
             </a>
         </div>
